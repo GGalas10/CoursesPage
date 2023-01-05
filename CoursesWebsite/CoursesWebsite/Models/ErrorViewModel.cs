96f83fdcdb4d279b9992b5204aea979734195b19
Grzegorz Galas <grzegorzgalas10@gmail.com>
2023-01-05 09:49:56 +0100

    Delete unnecessary project

--- CoursesWebsite/CoursesWebsite/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CoursesWebsite.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

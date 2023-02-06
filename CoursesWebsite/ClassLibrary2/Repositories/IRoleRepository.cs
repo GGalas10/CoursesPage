9c90314369d7e0b9402a1e1001e06293daad2dc4
Grzegorz Galas <grzegorzgalas10@gmail.com>
2023-02-06 15:26:27 +0100

    Add some code

--- CoursesWebsite/ClassLibrary2/Repositories/IRoleRepository.cs
+++ CoursesWebsite/ClassLibrary2/Repositories/IRoleRepository.cs
@@ -1,11 +1,12 @@
-﻿using Courses.Core.Models;
+﻿using APICore.Object_Value;
+using Courses.Core.Models;
 
 namespace Courses.Core.Repositories
 {
     public interface IRoleRepository
     {
         Task<string> GetUserRole(Guid userId);
-        Task CreateRoleAsync(string name);
+        Task CreateRoleAsync(Name name);
         Task DeleteRoleAsync(Role role);
         Task UpdateRoleAsync(Role role);
         Task AssignRole(Guid userId,Guid roleId);

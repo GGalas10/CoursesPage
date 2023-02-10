8e2e0ceff1edf7564ced083e4413b365e4ffc154
GGalas10 <grzegorzgalas10@gmail.com>
2023-02-10 16:00:47 +0100

    Adding hash and salt.

---
+++ CoursesWebsite/Courses.Infrastructure/Sercurity/SecurityClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Infrastructure.Sercurity
+{
+    internal static class SecurityClass
+    {
+        public static string CreateSalt(Guid id)
+        {
+            byte[] salt = Encoding.Default.GetBytes(Convert.ToString(id));
+            RandomNumberGenerator.Create().GetNonZeroBytes(salt);
+            return Convert.ToBase64String(salt);
+        }
+        public static string HashPassword(string password,string salt)
+        {
+            byte[] saltedpassword = Encoding.Default.GetBytes(String.Concat(salt, password));
+            var hash = SHA512.Create().ComputeHash(saltedpassword);
+            return Convert.ToBase64String(hash);
+        }
+        public static bool ComparePassword(string Dbpassword,string password,string salt)
+        {
+            var hasedpassword = HashPassword(password,salt);
+            return hasedpassword == Dbpassword;
+        }
+    }
+}

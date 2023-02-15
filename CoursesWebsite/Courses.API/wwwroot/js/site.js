36e25ce57dd4060dc0ec6100b4a1337cc8727e91
Grzegorz Galas <grzegorzgalas10@gmail.com>
2023-02-15 22:09:34 +0100

    adding some if in script.

--- CoursesWebsite/Courses.API/wwwroot/js/site.js
+++ CoursesWebsite/Courses.API/wwwroot/js/site.js
@@ -1,4 +1,45 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿/*<!-- Password check --> */
+const passwordInput = document.getElementById('RegisterPassword');
+passwordInput.addEventListener("input", function () {
+    let password = passwordInput.value;
+    let smallBox = document.getElementById('SmallSquare');
+    let test = 0;
+    let hasNumber = /\d/;
+    let hasUpperCase = /[A-Z]/;
+    let hasSpecialCharacter = /\W/;   
+    if (hasNumber.test(password))
+        test++;
+    if (hasUpperCase.test(password))
+        test++;
+    if (hasSpecialCharacter.test(password))
+        test++;
+    if (password.length <= 8)
+        test = 0;
+    else
+        test++;
+    if (test == 0) {
+        smallBox.innerHTML = "";
+        smallBox.style.width = "0%";
+    };
+    if (test == 1) {
+        smallBox.style.backgroundColor = "red";
+        smallBox.style.width = "25%";
+        smallBox.innerHTML = "Słabe hasło";
+    };
+    if (test == 2) {
+        smallBox.style.backgroundColor = "orange";
+        smallBox.style.width = "50%";
+        smallBox.innerHTML = "Średnie hasło";
+    };
+    if (test == 3) {
+        smallBox.style.backgroundColor = "green";
+        smallBox.style.width = "75%";
+        smallBox.innerHTML = "Silne hasło";
+    };
+    if (test == 4) {
+        smallBox.style.backgroundColor = "darkgreen";
+        smallBox.style.width = "100%";
+        smallBox.innerHTML = "Bardzo sile hasło";
+    };
+});
+/*<!-- Password check --> */
--- CoursesWebsite/Courses.Infrastructure/Comands/User/Register.cs
+++ CoursesWebsite/Courses.Infrastructure/Comands/User/Register.cs
@@ -1,12 +1,16 @@
 ﻿using Courses.Core.Value_Object;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Courses.Infrastructure.Comands.User
 {
     public class Register
     {
         public string UserName { get; set; }
+        [EmailAddress]
         public Email UserEmail { get; set; }
         public string? Login { get; set; }
+        [PasswordPropertyText]
         public Password Password { get; set; }
 
     }

beb6dbdbeea5af2f9a5a3bfed248d5a59bd92bee
Grzegorz Galas <grzegorzgalas10@gmail.com>
2023-02-18 23:02:56 +0100

    Adding admin panel with view

---
+++ CoursesWebsite/Courses.API/Controllers/PAdminController.cs
@@ -0,0 +1,38 @@
+﻿using Courses.Infrastructure.Comands.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Courses.API.Controllers
+{
+    [Route("API/PAdmin")]
+    public class PAdminController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        [HttpGet("Index")]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet("Login")]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+        [HttpGet("Register")]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(Login command)
+        {
+            return View();
+        }
+        public async Task<IActionResult> Register(Register command)
+        {
+
+            return View();
+        }
+    }
+}

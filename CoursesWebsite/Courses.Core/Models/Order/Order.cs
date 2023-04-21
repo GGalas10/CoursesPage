24e79de516b9db570c2cb075595811ef4b52b481
GGalas10 <grzegorzgalas10@gmail.com>
2023-04-21 09:17:10 +0200

    Continuation of the thought of DDD

--- CoursesWebsite/Courses.Core/Models/Order/Order.cs
+++ CoursesWebsite/Courses.Core/Models/Order/Order.cs
@@ -9,5 +9,31 @@
         public DateTime UpdatedAt { get;protected set; }
         public OrderStatus Status { get; protected set; }
         public IEnumerable<OrderCourses> OrderCourses => _Courses;
+        private Order() { }
+        public Order(Guid userId,List<OrderCourses> courses)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            AddCourses(courses);
+            Status = OrderStatus.Created;
+        }
+        public void AddCourse(OrderCourses course)
+        {
+            if (course == null)
+                throw new Exception("Course cannot be empty");
+            _Courses.Add(course);
+            UpdatedAt = DateTime.Now;
+        }
+        public void AddCourses(List<OrderCourses> courses)
+        {
+            if (courses.Count <= 0)
+                throw new Exception("Courses list cannot be empty");
+            _Courses.AddRange(courses);
+            UpdatedAt = DateTime.Now;
+        }
+        public void ChangeStatus(OrderStatus status)
+        {
+            Status = status;
+        }
     }
 }
--- CoursesWebsite/Courses.Core/Models/Order/OrderCourses.cs
+++ CoursesWebsite/Courses.Core/Models/Order/OrderCourses.cs
@@ -1,9 +1,28 @@
-﻿namespace Courses.Core.Models.Order
+﻿using Courses.Core.Value_Object;
+
+namespace Courses.Core.Models.Order
 {
     public class OrderCourses
     {
         public Guid Id { get; protected set; }
         public Guid OrderId { get; protected set; }
+        public Name Name { get; protected set; }
         public double Price { get; protected set; }
+        private OrderCourses() { }
+        public OrderCourses(double price,string name)
+        {
+            SetName(name);
+            SetPrice(price);
+        }
+        public void SetPrice(double price)
+        {
+            if (price < 0)
+                throw new Exception("Price cannot be less than 0");
+            Price = price;
+        }
+        public void SetName(Name name)
+        {
+            Name = name;
+        }
     }
 }

94b4c1e991e93305c052b1c1ceeec9256798cfaa
GGalas10 <grzegorzgalas10@gmail.com>
2023-04-21 15:26:36 +0200

    Rename.

---
+++ CoursesWebsite/Courses.Core/Models/Billing/Buyer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Core.Models.Billing
+{
+    public class Buyer
+    {
+    }
+}
---
+++ CoursesWebsite/Courses.Core/Models/Billing/InvoicingCourses.cs
@@ -0,0 +1,31 @@
+﻿using Courses.Core.Value_Object;
+
+namespace Courses.Core.Models.Billing
+{
+    public class BillingCourses
+    {
+        public Guid CourseId { get; protected set; }
+        public Guid BillId { get; protected set; }
+        public Name CourseName { get; protected set; }
+        public double Price { get; protected set; }
+        public virtual Bill Bill { get; protected set; }
+        private BillingCourses() { }
+        public BillingCourses(Guid courseId,string courseName,double price)
+        {
+            CourseId = courseId;
+            SetCourseName(courseName);
+            SetPrice(price);
+        }
+        public void SetCourseName(Name courseName)
+        {
+            if (string.IsNullOrEmpty(courseName))
+                throw new Exception("Name cannot be empty");
+            CourseName = courseName;
+        }
+        public void SetPrice(double price)
+        {
+            if (price < 0) throw new Exception("Price cannot be less than 0");
+            Price = Math.Round(price,2);
+        }
+    }
+}

f206dbe0e5bf409c5b13bfdc42fe7dfeb56cce8c
GGalas10 <grzegorzgalas10@gmail.com>
2023-04-25 15:20:35 +0200

    Add last class in core for Invoicing.

--- CoursesWebsite/Courses.Core/Models/Invoicing/Settings/InvoiceSettings.cs
+++ CoursesWebsite/Courses.Core/Models/Invoicing/Settings/InvoiceSettings.cs
@@ -3,12 +3,24 @@
     public class InvoiceSettings
     {
         public int InvoicingNumber { get; protected set; }
-        public string? InvoiceEnd { get; protected set; }
+        public string InvoiceEnd { get; protected set; }
         private InvoiceSettings() { }
         public InvoiceSettings(int invoicingNumber, string? invoiceEnd)
         {
-            InvoicingNumber = invoicingNumber;
-            InvoiceEnd = invoiceEnd;
+            SetNumber(invoicingNumber);
+            SetEnd(invoiceEnd);
+        }
+        public void SetNumber(int number)
+        {
+            if (number <= 0)
+                throw new Exception("Invoice number cannot be equal or less than 0");
+            InvoicingNumber = number;
+        }
+        public void SetEnd(string? end)
+        {
+            if(string.IsNullOrEmpty(end))
+                InvoiceEnd ="/"+DateTime.Now.Year.ToString();
+            InvoiceEnd = end;
         }
     }
 }
---
+++ CoursesWebsite/Courses.Core/Repositories/IInvoiceRepository.cs
@@ -0,0 +1,18 @@
+﻿using Courses.Core.Models.Invoicing;
+using Courses.Core.Models.Invoicing.Settings;
+
+namespace Courses.Core.Repositories
+{
+    public interface IInvoiceRepository
+    {
+        Task CreateInvoice(Invoice invoice);
+        Task<Invoice> GetInvoiceByIdAsync(Guid invoiceId);
+        Task<List<Invoice>> GetUserInvoicesAsync(Guid userId);
+        Task<List<Invoice>> GetRecipentInvoicesAsync(Guid recipentId);
+        Task<List<Invoice>> GetBuyerInvoicesAsync(Guid buyerId);
+        Task EditInvoiceAsync(Invoice invoice);
+        Task UpdateInvoiceAsync();
+        Task<InvoiceSettings> GetInvoiceSettings();
+
+    }
+}

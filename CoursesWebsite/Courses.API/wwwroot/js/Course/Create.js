3163ba48079f3f64cbd660ecc69d355485f54126
GGalas10 <grzegorzgalas10@gmail.com>
2024-02-15 23:54:48 +0100

    Course create View in production

--- CoursesWebsite/Courses.API/wwwroot/js/Course/Create.js
+++ CoursesWebsite/Courses.API/wwwroot/js/Course/Create.js
@@ -1,6 +1,54 @@
-function firstPanelDone() {
+function FirstPanelDone() {
+    FirstPanelToSummary();
     document.getElementById("FirstPanel").style.transform = `translateX(-96%)`;
 }
 function ReturnToFirstPanel() {
     document.getElementById("FirstPanel").style.transform = `translateX(0)`;
+}
+function SecondPanelDone() {
+    SecondPanelToSummary();
+    document.getElementById("SecondPanel").style.transform = `translateX(-96%)`;
+}
+function ReturnToSecondPanel() {
+    document.getElementById("SecondPanel").style.transform = `translateX(0)`;
+}
+function ReturnToLastPanel() {
+    document.getElementById("LastPanel").style.transform = `translateX(0)`;
+}
+function GoToSummary() {
+    LastPanelToSummary();
+    document.getElementById("LastPanel").style.transform = `translateX(-96%)`;
+}
+function addImageToPreview() {
+
+}
+document.getElementById("Picture").addEventListener('change', AddImage);
+function AddImage() {
+    if (!this.files || !this.files[0]) return;
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function (evt) {
+        console.log("test");
+        document.getElementById("PictureView").style.backgroundImage = `url('${evt.target.result}')`;
+        document.getElementById("PictureView").style.backgroundRepeat = `no-repeat`;
+        document.getElementById("PictureView").style.backgroundPosition = `center`;
+        document.getElementById("PictureView").style.backgroundSize = `cover`;
+        document.getElementById("PictureView").style.backdropFilter = 'blur(0)';
+
+    });
+    reader.readAsDataURL(this.files[0]);
+}
+function FirstPanelToSummary() {
+    document.getElementById("SummaryTitle").innerHTML = document.getElementById("Title").value;
+    document.getElementById("SummaryDescription").innerHTML = document.getElementById("Description").value;
+}
+function SecondPanelToSummary() {
+    document.getElementById("SummaryPicture").style.backgroundImage = document.getElementById("PictureView").style.backgroundImage;
+    document.getElementById("SummaryPicture").style.backgroundRepeat = document.getElementById("PictureView").style.backgroundRepeat;
+    document.getElementById("SummaryPicture").style.backgroundPosition = document.getElementById("PictureView").style.backgroundPosition;
+    document.getElementById("SummaryPicture").style.backgroundSize = document.getElementById("PictureView").style.backgroundSize;
+}
+function LastPanelToSummary() {
+    document.getElementById("SummaryPrice").innerHTML = document.getElementById("Price").value + " zł";
+    document.getElementById("SummaryAuthor").innerHTML = document.getElementById("Author").value;
 }
